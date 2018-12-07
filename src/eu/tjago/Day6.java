6311c099c525f0ca072d1acd23c6e85dbf8c001a
Tomasz Jagodowski <jagodowski.tomasz@gmail.com>
2018-12-07 12:21:32 +0000

    Day6: grit instantiation

--- src/eu/tjago/Day6.java
+++ src/eu/tjago/Day6.java
@@ -2,6 +2,7 @@ package eu.tjago;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -20,11 +21,15 @@ public class Day6 {
                 .collect(Collectors.toList());
 
 
-        System.out.println(locations);
+        System.out.println(new Grid().initGrid(locations));
+
+        //loop on Grid and set coordinates values to closest locations index values
+
     }
 
     class Location {
         int x, y;
+        boolean cornerLocation;
 
         Location(int x, int y) {
             this.x = x;
@@ -40,13 +45,51 @@ public class Day6 {
         }
     }
 
+    class Grid {
+        int maxLength, maxWidth;
+
+        Integer array[][];
+
+        Grid() {}
+
+        Grid(int maxLength, int maxWidth) {
+            this.maxLength = maxLength;
+            this.maxWidth = maxWidth;
+            array = new Integer[maxLength][maxWidth];
+        }
+
+        Grid initGrid(List<Location> locations) {
+            Optional<Integer> maxX = locations.stream()
+                    .map(location -> location.x)
+                    .max(Integer::compareTo);
+
+            Optional<Integer> maxY = locations.stream()
+                    .map(location -> location.y)
+                    .max(Integer::compareTo);
+
+            if (maxX.isPresent() && maxY.isPresent()) {
+                return new Grid(maxX.get(), maxY.get());
+            }
+
+            throw new RuntimeException("Could not instantiate new Grid");
+
+        }
+
+        @Override
+        public String toString() {
+            return "Grid{" +
+                    "maxLength=" + maxLength +
+                    "maxWidth=" + maxWidth +
+                    '}';
+        }
+    }
+
     /**
-     * Algirthm
+     * Algorithm concept
+     *
      * 1. fill locations
      * 2. find location closest points
-     * 3. calculate the grid map
-     *
-     * v2
-     * loop all grid coordinates, for every
+     * 3. create Grid
+     * 4. loop all grid coordinates, for every point find closest Loc
      */
 }
